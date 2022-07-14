3b1def312f2fd93de617d39a433736d683b34e6b
mathiconical <netinho340@gmail.com>
2022-07-13 21:17:23 -0300

    diagonal_p array_matrix

---
+++ maths/mat4.cpp
@@ -0,0 +1,21 @@
+#include "mat4.h"
+
+mat4::mat4()
+{
+	for (int i{ 0 }; i < 4 * 4; i++)
+		elements[i] = 0.0f;
+}
+
+mat4::mat4(float diagonal)
+{
+	//! i == ( b * s ) - 1
+	int j{ 0 }, k{ 0 };
+	for (int i{ 0 }; i < 4 * 4; i++) {
+		if (i == (4 * j + k)) {
+			elements[i] = 1;
+			++j; ++k;
+		}
+		else
+			elements[i] = 0;
+	}
+}
